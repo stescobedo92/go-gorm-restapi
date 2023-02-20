01d61004bc3bc751d6cc902963ac912b03eb1ebc
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2023-02-20 17:55:53 -0600

    Executing db connection function

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/stescobedo92/go-gorm-restapi/db"
 	"github.com/stescobedo92/go-gorm-restapi/routes"
 )
 
 func main() {
+	db.DBConnection()
+
 	router := mux.NewRouter()
 	router.HandleFunc("/", routes.HomeHandler)
 
