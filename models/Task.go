48ad254f578794755f4a41bbb9b6b27ef702e2b7
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2023-02-20 23:34:39 -0600

    Added model for Task

--- models/Task.go
+++ models/Task.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Task struct {
 	gorm.Model
 
-	Title       string `gorm:"not null;unique_index"`
+	Title       string `gorm:"type:varchar(100);not null;unique_index"`
 	Description string
 	Done        bool `gorm:"default:false"`
 	UserID      uint
