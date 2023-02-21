b13a62d402bd1464f576d13130aa81872d3be106
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2023-02-20 18:11:21 -0600

    Added execute gorm migrations

--- main.go
+++ main.go
@@ -5,11 +5,15 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/stescobedo92/go-gorm-restapi/db"
+	"github.com/stescobedo92/go-gorm-restapi/models"
 	"github.com/stescobedo92/go-gorm-restapi/routes"
 )
 
 func main() {
 	db.DBConnection()
+
+	db.DB.AutoMigrate(models.Task{})
+	db.DB.AutoMigrate(models.User{})
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", routes.HomeHandler)
